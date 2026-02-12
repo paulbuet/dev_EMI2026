d6b659d88ee24ce9e7a07ff4f7b8890f4b1c37a0
paul <paul.buet@meteo.fr>
2026-02-12 16:41:59 +0100

    Ajout d'argparse et début de fonctionnement du box_langrangien

---
+++ Scripts/box_langrangien.py
@@ -0,0 +1,16 @@
+
+from fonctions import Initial_Cond
+
+class Model():
+
+    def __init__(self, type_advance,number_stitches,deformable):
+
+        self.type_advance = type_advance
+        self.number_stitches = number_stitches
+        self.deformable = deformable
+
+    def run():
+        
+
+
+        
