41d6775a598c256e0fcef29537017c113dee2019
beslon <samuel.beslon@meteo.fr>
2026-02-12 11:26:43 +0100

    Ajout scripts code et fonctions

---
+++ Scripts/fonctions.py
@@ -0,0 +1,6 @@
+# Le but de ce fichier est de mettre toutes les fonctions qu'on peut potentiellement appelés pour faire tourner nos futur codes
+# Ensuite il suffira de faire un appelle du fichier et de la fonction qu nous interesse
+# Exemple avec cette fonction test que j'appelle dans le code Sedimentation.py
+class equation :
+    def test() :
+        return 'toto'
