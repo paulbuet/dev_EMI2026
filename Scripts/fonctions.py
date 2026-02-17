561c0519891a0b1a41fbb015dc665bc43cae2a38
arthur.deroissart@meteo.fr <arthur.deroissart@meteo.fr>
2026-02-17 13:56:54 +0100

    ajout de la fonctionnalité par bins + outils de plots + meilleure présentation dans InitialCond

--- Scripts/fonctions.py
+++ Scripts/fonctions.py
@@ -119,19 +119,32 @@ class InitialCond :
     Class that poduces a dataset to initialize the sedimentation model, a type of initialization is used in a restricted area of the vertical gridding.
     The dataset contains the concentration profile of the specie for each bin of the sedimentation scheme.
 
-    Attributes
+    Parameters
     ----------
     nb_grid : number of grid cells in the model
+    esp : specie considered "i", "s", "g", "r" or "c"
     mode : type of intial condition used, by default the last grid cell contains a relative concentration of 1
-    area_init : area in which the relative specie concentration is initialized, by default the initialization area is [30,50]
-    bin_concentration : concentrations of the specie in each bin, by default only one bin is considered and its concentration is 1
+    Hmax : atltitude of the maximum of the gaussian fonction of the concentration if the type of initial condition is "gauss"
+    sigma : value of the sigma parameter used for the gaussian function if the type of initial condition is "gauss"
+    
+
+    Attributes
+    ----------
+    self.levels_boundaries : list containing the altitudes of the boundaries of each grid mesh point
+    self.grid : list containing the grid points
+    self.data : DataArray having for variables
+        concentration_bin_{i} : the concentrations of the specie in the i-th bin over the vertical grid
+        diameter_bin_{i} : the diameter of the specie for the i-th bin
+    and for coordinates 
+        level : the levels of the grid points
 
     '''
     
-    def __init__(self, nb_grid, esp, mode = "simple", Hmax = 5000, sigma = 2000, bin_concentration = [5]) :
+    def __init__(self, nb_grid, esp, mode = "simple", Hmax = 5000, sigma = 2000, nb_classes = 10, rho_r = 1, N = 1) :
 
         if nb_grid == "ARO" : 
-            self.levels_boundaries = np.array([5.00148256575414, 16.7609146275979, 31.9999856716034, 50.6506387418972, 72.6448134875948, 97.9144556307367, 126.391508411840, 158.007915671418, 192.695621996127, 230.386571302649, 271.012706897240, 314.505973414367, 360.798314810016, 409.821674828922, 461.507997847950, 515.890042408161, 573.093937517738, 633.231198491812, 696.398736766220, 762.678848044096, 832.139215500780, 904.832909766711, 980.798389785428, 1060.05950095623, 1142.62547636213, 1228.49093654830, 1317.63588971212, 1410.02573054417, 1505.73432230725, 1604.93984760156, 1707.79812299161, 1814.44258334521, 1924.98428740911, 2039.51193175523, 2280.76794378598, 2407.56182949566, 2538.47269089309, 2673.47735336959, 2812.53026865253, 2955.56351459630, 3102.48360541930, 3253.19498943504, 3407.62661862652, 3565.73195622143, 3727.48898443499, 3892.90019410005, 4061.99258757885, 4234.81767907587, 4411.45149612596, 4591.99457746971, 4776.57197432233, 4965.33325159684, 5158.45249292134, 5356.12828712931, 5558.65852457596, 5766.45816179863, 5980.00284181189, 6199.82890110527, 6426.53336977965, 6660.77397670184, 6903.26915353359, 7154.79804227343, 7416.20049682756, 7688.37709125546, 7972.28912861375, 8268.07660797884, 8575.22917035383, 8893.62412699775, 9223.52646472597, 9565.58886389478, 9920.85173976858, 10290.7432801852, 10677.0795176319, 11082.0546910758, 11510.3223903101, 11966.1795634141, 12454.7528314299, 12982.0128918015, 13554.6557230282, 14180.1025765563, 14866.4999945363, 15627.4731252487, 16485.0471290191, 17467.8152385289, 18610.9387773901, 19955.6070022098, 21550.0160473017, 23450.1477297912, 34461.1214067193])
+            self.levels_boundaries = [5.00148256575414, 16.7609146275979, 31.9999856716034, 50.6506387418972, 72.6448134875948, 97.9144556307367, 126.391508411840, 158.007915671418, 192.695621996127, 230.386571302649, 271.012706897240, 314.505973414367, 360.798314810016, 409.821674828922, 461.507997847950, 515.890042408161, 573.093937517738, 633.231198491812, 696.398736766220, 762.678848044096, 832.139215500780, 904.832909766711, 980.798389785428, 1060.05950095623, 1142.62547636213, 1228.49093654830, 1317.63588971212, 1410.02573054417, 1505.73432230725, 1604.93984760156, 1707.79812299161, 1814.44258334521, 1924.98428740911, 2039.51193175523, 2280.76794378598, 2407.56182949566, 2538.47269089309, 2673.47735336959, 2812.53026865253, 2955.56351459630, 3102.48360541930, 3253.19498943504, 3407.62661862652, 3565.73195622143, 3727.48898443499, 3892.90019410005, 4061.99258757885, 4234.81767907587, 4411.45149612596, 4591.99457746971, 4776.57197432233, 4965.33325159684, 5158.45249292134, 5356.12828712931, 5558.65852457596, 5766.45816179863, 5980.00284181189, 6199.82890110527, 6426.53336977965, 6660.77397670184, 6903.26915353359, 7154.79804227343, 7416.20049682756, 7688.37709125546, 7972.28912861375, 8268.07660797884, 8575.22917035383, 8893.62412699775, 9223.52646472597, 9565.58886389478, 9920.85173976858, 10290.7432801852, 10677.0795176319, 11082.0546910758, 11510.3223903101, 11966.1795634141, 12454.7528314299, 12982.0128918015, 13554.6557230282, 14180.1025765563, 14866.4999945363, 15627.4731252487, 16485.0471290191, 17467.8152385289, 18610.9387773901, 19955.6070022098, 21550.0160473017, 23450.1477297912, 34461.1214067193]
+            nb_grid = 89
         else :
             height_grid = 12e3
             self.levels_boundaries = np.linspace(1/nb_grid * height_grid, height_grid, nb_grid)
@@ -149,24 +162,36 @@ class InitialCond :
             concentration_profile = [gaussienne(Hmax, sigma, self.grid[i]) for i in range(nb_grid)]
 
         eq=Eq(esp)
-        bin_concentration = Classe_D
+        lam = eq.Lanbda (rho_r, N)
+        dmin, dmax = eq.Dmin_Dmax(lam)
+        bin_concentration = eq.Classe_D (nb_classes, dmin, dmax, N, lam) # division in n bins
         
-        bin_profile = [np.array(concentration_profile) * bin_concentration[i] for i in range(len(bin_concentration))]
-        data_vars = {f"concentration_bin_{ind_bin+1}" : ("level", bin_profile[ind_bin]) for ind_bin in range(len(bin_concentration))}
-        self.data = xr.Dataset(data_vars=data_vars, coords = {"level" : self.grid})
+        bin_profile = [np.array(concentration_profile) * bin_concentration[i][1] for i in range(len(bin_concentration))] # computinng of the n bin profiles
+        data_vars1 = {f"concentration_bin_{ind_bin+1}" : ("level", bin_profile[ind_bin]) for ind_bin in range(len(bin_concentration))}
+        
+        data_vars2 = {f"diameter_bin_{ind_bin+1}" : bin_concentration[ind_bin][0] for ind_bin in range(len(bin_concentration))} # addition of the diameters
+        data_vars1.update(data_vars2)
+
+        self.data = xr.Dataset(data_vars= data_vars1, coords = {"level" : self.grid})
 
 ### Tests and verifications ###
 
-start = time.time()
-initial_conds = InitialCond(nb_grid = 50, esp = "r" mode = "gauss", bin_concentration = [1,2,3,4,5])
+#start = time.time()
+#initial_conds = InitialCond(nb_grid = "ARO", esp = "r", mode = "gauss", nb_classes = 10, rho_r = 100, N = 10)
 
-print (initial_conds.levels_boundaries)
-print (initial_conds.grid)
-print (initial_conds.data)
-print (initial_conds.data["concentration_bin_1"])
-print (initial_conds.data["level"])
+#print (initial_conds.levels_boundaries)
+#print (initial_conds.grid)
+#print (initial_conds.data)
+#print (initial_conds.data["concentration_bin_1"])
+#print (initial_conds.data["level"])
 
-c = initial_conds.data["concentration_bin_1"]
-z = initial_conds.grid
-plt.plot(c,z)
-plt.show()
+#z = initial_conds.grid
+#for i in range(10): 
+    #c = initial_conds.data[f"concentration_bin_{i+1}"]
+    #plt.plot(c,z)
+#plt.show()
+
+#d = [float(initial_conds.data[f"diameter_bin_{i+1}"]) for i in range(10)]
+#x = np.linspace(1,10,10)
+#plt.plot(x,d)
+#plt.show()
