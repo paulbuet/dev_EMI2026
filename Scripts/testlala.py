43c09af63757810a6cc8b8b568b2526042e356d6
paul <paul.buet@meteo.fr>
2026-03-17 16:47:30 +0100

    essai2

--- Scripts/testlala.py
+++ Scripts/testlala.py
@@ -18,21 +18,21 @@ def calcul_maille_arrivee(h1, h2, h3, h4, N, type, dt, lam) :
         #N : concentration actuelle dans la maille
         #type : choisir "concentration" pour faire tomber la concentration ou "masse" pour la masse
         #dt : pas de temps
-        f = lambda D,h : alpha/gamma(nu)*lam**(alpha*nu)*D**(alpha*nu-1)  * np.exp(-(lam*D)**alpha)
+        f = lambda D,h,sigma,beta,N : N*sigma*D**beta*alpha/gamma(nu)*lam**(alpha*nu)*D**(alpha*nu-1)  * np.exp(-(lam*D)**alpha)
 
 
         if type == "concentration" : 
-            sigma = 1
-            beta = 0
+            integrale = -dblquad(f, h3, h4, lambda h : (((h-h1)/(dt*c))**(1/d)), lambda h : (((h-h2)/(dt*c))**(1/d)),args=(1,0,N))[0]
+            print('val int : ', integrale)
+            new_val = integrale/(h2-h1)
+
+            return new_val
 
         if type == "masse" :
-            sigma = a
-            beta = b
+            integrale = -dblquad(f, h3, h4, lambda h : (((h-h1)/(dt*c))**(1/d)), lambda h : (((h-h2)/(dt*c))**(1/d)),args=(a,b,N))[0]
+            print('val int : ', integrale)
+            new_val = integrale/(h2-h1)
 
-        integrale = -dblquad(f, h3, h4, lambda h : (((h-h1)/(dt*c))**(1/d)), lambda h : (((h-h2)/(dt*c))**(1/d)))[0]
-        print('val int : ', integrale)
-        new_val = integrale/(h2-h1)
-
-        return new_val
+            return new_val
 
 print(calcul_maille_arrivee(0,10,15,20,1000,"concentration",10,300))
