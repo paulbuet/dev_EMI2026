484acad7a18c9f22dc3f0f0cc233e4c636cfd55e
arthur.deroissart@meteo.fr <arthur.deroissart@meteo.fr>
2026-03-25 11:27:58 +0100

    refonte de l'affichage pour faciliter l'enregistrement des figures

--- Scripts/ditribution.py
+++ Scripts/ditribution.py
@@ -19,15 +19,12 @@ from phyex import Eule, Eule2, Stat
 import numpy as np
 import time
 from pathlib import Path
-import os, sys
+import sys
 import matplotlib.pyplot as plt
 
 
 class distribution:
     def __init__(self,model,type_advance,number_stitches,deformable,number_bin,mixing_ratio,time_step,speed_max,esp,CFL, efficiency_test, type_init, path_phyex, path_fig, diag):
-
-        chemin= os.path.join(f"./fig",f"{model}", f"{type_advance}", f"{deformable}" )
-        os.makedirs(chemin, exist_ok=True)
         
         path_to_phyex = Path(path_phyex) / "PHYEX"
         sys.path.append(str(path_to_phyex))
@@ -91,69 +88,63 @@ class distribution:
                     if deformable == "No":     #  Par défaut on arrive ici.
                         a = time.time()
                         model_config = Model_bl(number_stitches,number_bin,mixing_ratio,time_step,speed_max,esp,CFL,type_init)
+                        b = time.time()
 
                         results = model_config.run()
                         concentration_formate = np.array(results[0]).sum(axis=1)
-                        # print(results[0])
-                        b = time.time()
                         mass_form = np.array(results[2]).sum(axis=1)
-                        Affichage.Affichage_Concentration(concentration_formate, "Concentration", model, path_fig,type_advance,deformable)
-                        Affichage.Affichage_Concentration(mass_form, "Masse", model, path_fig,type_advance,deformable)
-                        Affichage.Affichage_Precipitation(results[1], model,path_fig,type_advance,deformable)
-                        Affichage.Afficher()
+
+                        fig_config = Affichage(model, path_fig,type_advance,deformable)
+                        fig_config.afficher(concentration_formate,mass_form,results[1])
+
                     else:
                         a = time.time()
                         model_config = model_bl_def(number_stitches,time_step,esp,mixing_ratio,type_init)
+                        b = time.time()
 
                         results = model_config.run()
                         concentration_formate = np.array(results[0])
-                        b = time.time()
                         mass_form = np.array(results[2])
-                        Affichage.Affichage_Concentration(concentration_formate, "Concentration", model, path_fig,type_advance,deformable)
-                        Affichage.Affichage_Concentration(mass_form, "Masse", model, path_fig,type_advance,deformable)
-                        Affichage.Affichage_Precipitation(results[1], model,path_fig,type_advance,deformable)
-                        Affichage.Afficher()
+
+                        fig_config = Affichage(model, path_fig,type_advance,deformable)
+                        fig_config.afficher(concentration_formate,mass_form,results[1])
 
                 else:
                     if deformable == "No":
                         a = time.time()
                         model_config = Model_bl_sf(number_stitches,number_bin,mixing_ratio,time_step,speed_max,esp,CFL,type_init)
+                        b = time.time()
 
                         results = model_config.run()
                         concentration_formate = np.array(results[0]).sum(axis=1)
-                        b = time.time()
                         mass_form = np.array(results[2]).sum(axis=1)
-                        Affichage.Affichage_Concentration(concentration_formate, "Concentration", model, path_fig,type_advance,deformable)
-                        Affichage.Affichage_Concentration(mass_form, "Masse", model, path_fig,type_advance,deformable)
-                        Affichage.Affichage_Precipitation(results[1], model,path_fig,type_advance,deformable)
-                        Affichage.Afficher()
+
+                        fig_config = Affichage(model, path_fig,type_advance,deformable)
+                        fig_config.afficher(concentration_formate,mass_form,results[1])
+
                     else:
                         a = time.time()
                         model_config = model_bl_def_sf(number_stitches,time_step,esp,mixing_ratio,type_init)
+                        b = time.time()
 
                         results = model_config.run()
                         concentration_formate = np.array(results[0])
-                        b = time.time()
                         mass_form = np.array(results[2])
-                        Affichage.Affichage_Concentration(concentration_formate, "Concentration", model, path_fig,type_advance,deformable)
-                        Affichage.Affichage_Concentration(mass_form, "Masse", model, path_fig,type_advance,deformable)
-                        Affichage.Affichage_Precipitation(results[1], model,path_fig,type_advance,deformable)
-                        Affichage.Afficher()
+                        
+                        fig_config = Affichage(model, path_fig,type_advance,deformable)
+                        fig_config.afficher(concentration_formate,mass_form,results[1])
+
             elif model in ('EULE', 'EULE2', 'STAT'):
                 a = time.time()
                 cls = {'EULE': Eule, 'EULE2': Eule2, 'STAT': Stat}[model]
-                #print (cls)
                 model_config = cls(number_stitches,number_bin,time_step,speed_max,esp,CFL,type_init)
-                #model_config_bl = Model_bl_sf(number_stitches,number_bin,time_step,speed_max,esp,CFL,type_init)
 
                 results = model_config.run()
-
                 concentration_formate = np.array(results[2])
-                Affichage.Affichage_Concentration(concentration_formate, "Concentration", model, path_fig, type_advance, deformable)
                 mass_form = np.array(results[1])
-                Affichage.Affichage_Concentration(mass_form, "Masse", model, path_fig, type_advance, deformable)
-                Affichage.Affichage_Precipitation(results[0], model, path_fig, type_advance, deformable)
-                Affichage.Afficher()
+
+                fig_config = Affichage(model, path_fig,type_advance,deformable)
+                fig_config.afficher(concentration_formate,mass_form,results[0])
 
                 #print (f"{model_config.__dict__}")
                 #print (f"{model_config_bl.__dict__}")
